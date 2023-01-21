91d2542f2c26af5b57905526cf58989c97fc09de
jpatrick5402 <jpatrick5402@gmail.com>
2023-01-21 13:16:16 -0500

    Meets all requirements

--- Advanced Programming/recusion.cpp
+++ Advanced Programming/recusion.cpp
@@ -4,6 +4,7 @@
 // https://stackoverflow.com/questions/3775274/exponential-operator-in-c
 // https://stackoverflow.com/questions/3237247/c-rounding-to-the-nths-place
 // https://www.daniweb.com/programming/software-development/threads/211198/cout-padding-and-text-alignment#post947662
+// https://cplusplus.com/reference/iomanip/setprecision/
 
 #include <iostream>
 #include <cmath>
@@ -16,15 +17,15 @@ double interest;
 int loan_years;
 bool flag = true;
 
-void get_values(double &Prin, double &Intr, int &Years);
+void GetValues(double &Prin, double &Intr, int &Years);
 double PaymentCalculator(double &Principle, double &Interest, int &Years);
-void amortization(double &Principle, double &interest, int &loan_years, double &Pay_per_period);
-void amortization_recursion(int Payment_num, double Pay_per_period, double Interest, double Balance);
+void LoanAmortizaeScheduleHeading(double &Principle, double &interest, int &loan_years, double &Pay_per_period);
+void LoanAmortizaeSchedule(int Payment_num, double Pay_per_period, double Interest, double Balance);
 
 int main()
 {
 
-    get_values(principle, interest, loan_years);
+    GetValues(principle, interest, loan_years);
 
     double pay_per_period = PaymentCalculator(principle, interest, loan_years);
 
@@ -32,45 +33,65 @@ int main()
     system("cls");
     cout << "Joseph Patrick -- Lab 1 - Recursion" << endl
          << endl;
-    cout << "Principle (between $50,000 and $100,000): " << principle << endl;
+    cout << "Principle (between 50000 and 100000): " << principle << endl;
     cout << "Annual Interest Rate (e.g. 4.5): " << interest << endl;
     cout << "Loan Term in Years (10, 15, 25, or 40): " << loan_years << endl;
     system("pause");
 
-    amortization(principle, interest, loan_years, pay_per_period);
+    LoanAmortizaeScheduleHeading(principle, interest, loan_years, pay_per_period);
 }
 
-void get_values(double &Principle, double &Interest, int &Years)
+void GetValues(double &Principle, double &Interest, int &Years)
 {
     system("cls");
     cout << "Joseph Patrick -- Lab 1 - Recursion" << endl
          << endl;
 
-    // Looping to get accurate Principle amount
+    // Looping to get Principle amount
     do
     {
-        cout << "Principle (between $50,000 and $100,000): ";
+        cout << "Principle (between 50000 and 100000): ";
         cin >> Principle;
-        if (Principle <= 100000)
-        {
-            if (Principle >= 50000)
-            {
-                break;
-            }
-        }
+
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        if (Principle <= 100000 && Principle >= 50000)
+            break;
+
         system("cls");
-        cout << "Please enter a Principle between $50,000 and $100,000" << endl
+        cout << "Please enter a Principle between 50000 and 100000!" << endl
              << endl;
-    } while (flag);
+    } while (true);
 
     system("cls");
     cout << "Joseph Patrick -- Lab 1 - Recursion" << endl
          << endl;
-    cout << "Principle (between $50,000 and $100,000): " << Principle << endl;
+    cout << "Principle (between 50000 and 100000): " << Principle << endl;
 
-    // Getting Interest Rate
-    cout << "Annual Interest Rate (e.g. 4.5): ";
-    cin >> Interest;
+    // Looping to get Interest Rate
+    do
+    {
+        cout << "Annual Interest Rate (e.g. 4.5): ";
+        cin >> Interest;
+
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        if (Interest > 0 && Interest < 100)
+            break;
+        system("cls");
+        cout << "Please enter a valid interest rate!" << endl
+             << endl;
+        cout << "Principle (between 50000 and 100000): " << Principle << endl;
+
+    } while (true);
+
+    system("cls");
+    cout << "Joseph Patrick -- Lab 1 - Recursion" << endl
+         << endl;
+    cout << "Principle (between 50000 and 100000): " << Principle << endl;
+    cout << "Annual Interest Rate (e.g. 4.5): " << Interest << endl;
 
     // Looping to get Loan Term
     do
@@ -78,21 +99,16 @@ void get_values(double &Principle, double &Interest, int &Years)
         cout << "Loan Term in Years (10, 15, 25, or 40): ";
         cin >> Years;
 
-        switch (Years)
-        {
-        case 10:
-            flag = false;
-        case 15:
-            flag = false;
-        case 25:
-            flag = false;
-        case 40:
-            flag = false;
-        }
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        if (Years == 10 || Years == 15 || Years == 25 || Years == 40)
+            break;
+
         system("cls");
-        cout << "Please enter loan term option" << endl
+        cout << "Please enter loan term option!" << endl
              << endl;
-        cout << "Principle (between $50,000 and $100,000): " << Principle << endl;
+        cout << "Principle (between 50000 and 100000): " << Principle << endl;
         cout << "Annual Interest Rate (e.g. 4.5): " << Interest << endl;
     } while (flag);
 }
@@ -106,7 +122,7 @@ double PaymentCalculator(double &Principle, double &Interest, int &Years)
     return pay_per_period;
 }
 
-void amortization(double &Principle, double &Interest, int &Loan_years, double &Pay_per_period)
+void LoanAmortizaeScheduleHeading(double &Principle, double &Interest, int &Loan_years, double &Pay_per_period)
 {
     system("cls");
     cout << "Joseph Patrick -- Lab 1 - Recursion" << endl
@@ -116,26 +132,26 @@ void amortization(double &Principle, double &Interest, int &Loan_years, double &
     cout << setw(40) << left << "Principle: " << right << "$" << principle << endl;
     cout << setw(40) << left << "Annual Interest Rate: " << right << interest << "%" << endl;
     cout << setw(40) << left << "Life of Loan: " << right << loan_years << " years" << endl;
-    cout << setw(40) << left << "Payment per period: $" << right << Pay_per_period << endl
+    cout << setw(40) << left << "Payment per period:" << right << fixed << setprecision(2) << "$" << Pay_per_period << endl
          << endl;
 
     cout << setw(10) << right << "Payment" << setw(10) << "Amount" << setw(10) << "Interest" << setw(10) << "Principal" << setw(10) << "Balance" << endl;
     cout << setw(50) << right << setprecision(2) << fixed << Principle << endl;
-    amortization_recursion(1, Pay_per_period, Interest, Principle);
+    LoanAmortizaeSchedule(1, Pay_per_period, Interest, Principle);
 }
 
-void amortization_recursion(int Payment_num, double Pay_per_period, double Interest_rate, double Balance)
+void LoanAmortizaeSchedule(int Payment_num, double Pay_per_period, double Interest_rate, double Balance)
 {
     float Interest_cost = (Interest_rate / 1200) * Balance;
     float Principle = Pay_per_period - Interest_cost;
 
-    cout << setw(10) << right << fixed << setprecision(2) << Payment_num << setw(10) << Pay_per_period << setw(10) << Interest_cost << setw(10) << Principle << setw(10) << round(((Balance - Principle) * 100)) / 100 << endl;
+    cout << setw(10) << right << fixed << setprecision(2) << Payment_num << setw(10) << Pay_per_period << setw(10) << Interest_cost << setw(10) << Principle << setw(10) << abs(round(((Balance - Principle) * 100)) / 100) << endl;
 
     Balance = Balance - Principle;
 
     if ((round(Balance - Principle)) >= 0)
     {
         Payment_num++;
-        amortization_recursion(Payment_num, Pay_per_period, Interest_rate, Balance);
+        LoanAmortizaeSchedule(Payment_num, Pay_per_period, Interest_rate, Balance);
     }
 }
