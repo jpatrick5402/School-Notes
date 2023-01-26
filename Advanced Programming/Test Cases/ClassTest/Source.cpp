5c868a7eb7f76261a610faf4aadf3092e96f7ce9
jpatrick5402 <jpatrick5402@gmail.com>
2023-01-26 12:56:05 -0500

    Needed namespace std and int type

--- Advanced Programming/Test Cases/ClassTest/Source.cpp
+++ Advanced Programming/Test Cases/ClassTest/Source.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
+using namespace std;
 class carType
 {
 public:
-    carType(int, int, int);
-    int getMake(const carType &thisCar);
+    carType(string Make, string Model, int Miles);
+    int getMake();
     int getModel();
-    double getMiles();
+    int getMiles();
 
 private:
-    double miles;
+    int miles;
+    string make;
+    string model;
 };
+
+carType::carType(string Make, string Model, int Miles)
+{
+    miles = Miles;
+    make = Make;
+    model = Model;
+}
+
+int carType::getMiles()
+{
+    return miles;
+}
+
 int main()
 {
+    carType myCar("toyota", "tundra", 1200);
+
+    cout << myCar.miles;
 }
