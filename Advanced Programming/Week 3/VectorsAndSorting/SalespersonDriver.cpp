e83ac2242033d396bf7ca7f29be6dbe011aef585
jpatrick5402 <jpatrick5402@gmail.com>
2023-02-03 23:03:57 -0500

    working fileinput

--- Advanced Programming/Week 3/VectorsAndSorting/SalespersonDriver.cpp
+++ Advanced Programming/Week 3/VectorsAndSorting/SalespersonDriver.cpp
@@ -6,19 +6,35 @@ using namespace std;
 int main()
 {
 	char fileName[256];
+	char name[256];
+	char dealer[256];
+	int goal;
+	double commission;
+	int temp;
+
 	Salesperson MyPerson;
 	cout << "Enter the name of the file: ";
 	cin.getline(fileName, 256);
 
 	ifstream infile;
 	infile.open(fileName);
-	string name;
-	string dealer;
-	int goal;
-	double commission;
-	infile >> name;
-	infile >> dealer;
+
+	infile.getline(name,256);
+	MyPerson.setSalespersonName(name);
+	infile.getline(dealer, 256);
+	MyPerson.setDealershipName(dealer);
 	infile >> goal;
+	MyPerson.setSalesGoal(goal);
 	infile >> commission;
+	MyPerson.setCommissionPercentage(commission);
+	infile >> temp;
+	do
+	{
+		cout << temp;
+		MyPerson.addSalesAmount(temp);
+		infile >> temp;
+	} while (!infile.eof());
 	infile.close();
+
+	MyPerson.displaySalesInfo();
 }
