4f18426a076a1497ca2a19d497061ad74e8474db
jpatrick5402 <jpatrick5402@gmail.com>
2023-02-17 09:05:19 -0500

    Database queries and starting notes

---
+++ Advanced Programming/Week 5/notes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Pointer variable: A variable whose content is an address
+    // (that is, a memory address) and is therefore said to point
+    // to a memory address.
+
+    int *p; // declaring pointer also can be done with '*' anywhere
+    int *p;
+    int *p; // all valid
+
+    int *p, q; // pointer, int
+    int *p, *q // pointer, pointer
+
+            char *ch; // stores the address of a char variablee
+
+    // '&' is the address of operator
+    // '*' is the dereferencing operator
+}
