a6a46ebbe4c33d9eca5b8c45cf8ad05b25d6828a
jpatrick5402 <jpatrick5402@gmail.com>
2023-02-23 22:21:22 -0500

    Finalized Lab 6

--- Advanced Programming/Week 6/Lab6/DynamicMemory/BookDriver.cpp
+++ Advanced Programming/Week 6/Lab6/DynamicMemory/BookDriver.cpp
@@ -1,5 +1,6 @@
 #include "Book.h"
 #include <iomanip>
+#include <stdlib.h>
 using namespace std;
 
 // *****************************************************************************************
@@ -10,15 +11,18 @@ using namespace std;
 // management to prevent issues with memory leaks, security, and overall program performance
 // *****************************************************************************************
 
+int generateScore(int position);
 
 int main()
 {
+	cout << "Joseph Patrick - Lab 6" << endl << endl;
 	// Creating varibles to be used in main()
 	int bookNum;
 	string bookName;
 	string genreName;
 	string bookTypeName;
 	int DISP_WIDTH = 20;
+	int score;
 
 	// Getting the user's read books for the past year
 	cout << "Enter the number of books that have read in the previous year: ";
@@ -26,46 +30,69 @@ int main()
 
 	while (cin.fail()) // Looping if failed to get bookNum
 	{
-		cin.ignore();
 		cin.clear();
+		cin.ignore();
 		cout << "Please enter a number" << endl;
 		cout << "Enter the number of books that have read in the previous year: ";
 		cin >> bookNum;
 	}
 
-	cin.ignore();
 	cin.clear();
+	cin.ignore();
 
 	// Creating dynamic list of Books
 	// https://learn.microsoft.com/en-us/cpp/cpp/new-and-delete-operators?view=msvc-170
-	Book* bookList = new Book[bookNum];
+
+	Book** bookList = new Book * [bookNum];
 
 	for (int i = 0; i < bookNum; i++) // Loopring to get info for books
 	{
-		cout << "What is the name of the Book?" << endl;
+		cout << "What is the name of Book #" << i + 1 << " ?" << endl;
 		getline(cin, bookName);
 		cout << "What is the genre of the " << bookName << " book? (Western, Devotional, etc.)" << '\n';
 		getline(cin, genreName);
-		cout << "What is the type of the book? (Paper, Electronic, etc.)" << '\n';
+		cout << "What is the type of Book #" << i + 1 << " ? (Paper, Electronic, etc.)" << '\n';
 		getline(cin, bookTypeName);
 
-		bookList[i].setTitle(bookName);
-		bookList[i].setGenre(genreName);
-		bookList[i].setTypeBook(bookTypeName);
-		bookList[i].setReadingScore(1);
+		score = generateScore(i);
 
+		Book* p = new Book();
+
+		// I used the "->" from the Lab 6 Demo
+		p->setTitle(bookName);
+		p->setGenre(genreName);
+		p->setTypeBook(bookTypeName);
+		p->setReadingScore(score);
+
+		bookList[i] = p;
 		cout << '\n';
 	}
 
-	cout << "These are the books you have read this year" << '\n';
+	cout << "These are the books you have read this year:" << '\n';
 
+	for (int i = 0; i < bookNum; i++)  // Getting bad memory allocation error here, resolved by switching "(*bookList)[i]" to "bookList[i]->"
+	{
+		cout << setw(DISP_WIDTH) << left << "Book Name:" << bookList[i]->getTitle() << '\n';
+		cout << setw(DISP_WIDTH) << left << "Book Genre:" << bookList[i]->getGenre() << '\n';
+		cout << setw(DISP_WIDTH) << left << "Book Type:" << bookList[i]->getTypeBook() << '\n';
+		cout << setw(DISP_WIDTH) << left << "Reading Score (1-5): " << bookList[i]->getReadingScore() << '\n';
+		cout << '\n';
+	}
+
+	// From instructions
 	for (int i = 0; i < bookNum; i++)
 	{
-		cout << setw(DISP_WIDTH) << left << "Book Name:" << bookList[i].getTitle() << '\n';
-		cout << setw(DISP_WIDTH) << left << "Book Genre:" << bookList[i].getGenre() << '\n';
-		cout << setw(DISP_WIDTH) << left << "Book Type:" << bookList[i].getTypeBook() << '\n';
-		cout << setw(DISP_WIDTH) << left << "Reading Score (1-5): " << bookList[i].getReadingScore() << '\n';
-		cout << '\n';
+		delete bookList[i];
+		bookList[i] = NULL;
 	}
+	delete[] bookList;
 
+	system("pause");
+}
+
+int generateScore(int position)
+{
+	srand(100 + position);
+	int num = rand() % 5;
+	return num + 1;
 }
